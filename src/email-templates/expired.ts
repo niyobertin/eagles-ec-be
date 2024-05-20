b393153a9bf5b1316b6642852be49c0ddf36eb01
gisubizo Jovan <gisubizojovan@gisubizos-MacBook-Pro.local>
2024-05-20 17:02:10 +0200

    Feature: Unlist expired product

--- src/email-templates/expired.ts
+++ src/email-templates/expired.ts
@@ -7,16 +7,16 @@ export const expiredProductTemplate = (info: IInfo) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account Verification</title>
+    <title>📣 Product Expiry Notification 🔔</title>
   </head>
   <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color: #f8f9fa; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
     <div style="width: 80%; max-width: 400px; margin:auto; padding: 30px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 10px rgba(0,0,0,0.1); text-align: center;">
-      <h1 style="color: #333333; font-size: 24px; margin-bottom: 20px;">Product Expired ⌛️</h1>
-      <p style="color: #666666; font-size: 16px; line-height: 1.6; margin-bottom: 20px;">Dear Beloved client, we noticed that Your product ${info.name} has expired and it visbility to client has been disbaled, please consider updating it or delete it</p>
+      <h1 style="color: #333333; font-size: 24px; margin-bottom: 20px;">Your Product has Expired ⌛️</h1>
+      <p style="color: #666666; font-size: 16px; line-height: 1.6; margin-bottom: 20px;">Dear Beloved Seller, we noticed that Your product ${info.name} has expired and it's visbility to client has been disabled, please consider to update it or delete it</p>
       
     
       <div style="display: flex; justify-content: center; margin:auto;">
-        <p style="font-style: italic; color: #999999;">Happy Trading😎.</p>
+        <p style="font-style: italic; color: #999999;">Happy Trading 😎.</p>
       </div>
     </div>
   </body>
